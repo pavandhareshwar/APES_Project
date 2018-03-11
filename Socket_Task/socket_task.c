59dce1fcbe4f412c202b32522f789d3f03f495b6
Pavan Dhareshwar <pavan.dhareshwar@colorado.edu>
2018-03-10 20:46:35 -0700

    1. Added initial code to accept the request from external application, determine the recipient of the request, forward it to either temperature or light task, get the response back and sent it to external application. 2. Added new API's to create and initialize server and sensor sockets.

--- Socket_Task/socket_task.c
+++ Socket_Task/socket_task.c
@@ -2,11 +2,90 @@
 
 int main(void)
 {
-	/* Parent process */
 	int serv_addr_len = sizeof(server_addr);
 	char buffer[BUFF_SIZE];
 
-	// Creating a socket
+	/* Creating a socket that is exposed to the external application */
+    initialize_server_socket(&server_addr, SERVER_PORT_NUM, SERVER_LISTEN_QUEUE_SIZE);
+
+    initialize_sensor_task_socket(&temp_sockfd, &temp_sock_addr, TEMPERATURE_TASK_PORT_NUM);
+
+    if (connect(temp_sockfd, (struct sockaddr *)&temp_sock_addr, sizeof(temp_sock_addr)) < 0)
+    {
+        perror("connect failed");
+        printf("\nConnection Failed for temperature task \n");
+        return -1;
+    }
+   
+#if 0
+    initialize_sensor_task_socket(&light_sockfd, &light_sock_addr, LIGHT_TASK_PORT_NUM);
+
+    if (connect(light_sockfd, (struct sockaddr *)&light_sock_addr, sizeof(light_sock_addr)) < 0)
+    {
+        printf("\nConnection Failed for light task \n");
+        return -1;
+    }
+#endif
+
+	int accept_conn_id;
+	while(1){
+	
+        /* Wait for request from external application */
+		if ((accept_conn_id = accept(server_sockfd, (struct sockaddr *)&server_addr, 
+					   (socklen_t *)&serv_addr_len)) < 0)
+		{
+			perror("accept");
+		}
+	
+        char recv_buffer[BUFF_SIZE];
+        memset(recv_buffer, '\0', sizeof(recv_buffer));
+        int num_recv_bytes = recv(accept_conn_id, recv_buffer, sizeof(recv_buffer), 0);
+        if (num_recv_bytes < 0)
+        {
+            printf("recv failed in socket task\n");
+            perror("recv failed");
+        }
+        else
+        {
+            printf("Message req api: %s, req recp: %s, req api params: %s\n",
+                    (((struct _socket_req_msg_struct_ *)&recv_buffer)->req_api_msg),
+                    ((((struct _socket_req_msg_struct_ *)&recv_buffer)->req_recipient) 
+                     == REQ_RECP_TEMP_TASK ? "Temp Task" : "Light Task"),
+                    (((struct _socket_req_msg_struct_ *)&recv_buffer)->ptr_param_list != NULL ?
+                     ((struct _socket_req_msg_struct_ *)&recv_buffer)->ptr_param_list : "NULL"));
+            
+            memset(buffer, '\0', sizeof(buffer));
+            strncpy(buffer, "Hello!", strlen("Hello!"));
+            
+            size_t sent_bytes;
+            if ((((struct _socket_req_msg_struct_ *)&recv_buffer)->req_recipient) 
+                        == REQ_RECP_TEMP_TASK)
+            {
+                printf("Sending request to temperature task\n");
+                sent_bytes = send(temp_sockfd, recv_buffer, sizeof(recv_buffer), 0);
+
+                ssize_t num_recv_bytes = recv(temp_sockfd, buffer, sizeof(buffer), 0);
+                if (num_recv_bytes < 0)
+                    perror("recv failed");
+
+                sent_bytes = send(accept_conn_id, buffer, strlen(buffer), 0);
+            }
+            else
+            {
+            }
+
+            //sent_bytes = send(light_sockfd, buffer , sizeof(buffer) , 0);
+            //sent_bytes = send(accept_conn_id, buffer, sizeof(buffer), 0 );
+
+        }
+	}
+	return 0;
+}
+
+void initialize_server_socket(struct sockaddr_in *sock_addr_struct, 
+                                int port_num, int listen_queue_size)
+{
+	/* Create server socket */ 
 	if ((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
 		perror("socket failed");
@@ -14,107 +93,59 @@ int main(void)
 	}	
 
     int option = 1;
-    if(setsockopt(server_sockfd,SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0)
+    if(setsockopt(server_sockfd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR),
+                    (char*)&option,sizeof(option)) < 0)
     {
         printf("setsockopt failed\n");
         close(server_sockfd);
-        exit(2);
+        exit(EXIT_FAILURE);
     }
 
-    server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(SERVER_PORT_NUM);
+    sock_addr_struct->sin_family = AF_INET;
+	sock_addr_struct->sin_addr.s_addr = INADDR_ANY;
+	sock_addr_struct->sin_port = htons(port_num);
 
-	if (bind(server_sockfd, (struct sockaddr *)&server_addr, 
-							sizeof(server_addr))<0)
+	if (bind(server_sockfd, (struct sockaddr *)sock_addr_struct, 
+							sizeof(struct sockaddr_in))<0)
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
 
-	if (listen(server_sockfd, SERVER_LISTEN_QUEUE_SIZE) < 0)
+	if (listen(server_sockfd, listen_queue_size) < 0)
 	{
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-	
-	memset(&temp_sock_addr, '0', sizeof(temp_sock_addr));
-	temp_sock_addr.sin_family = AF_INET;
-	temp_sock_addr.sin_port = htons(TEMPERATURE_TASK_PORT_NUM);
+
+}
+
+void initialize_sensor_task_socket(int *sock_fd, struct sockaddr_in *sock_addr_struct, int port_num)
+{
+	memset(sock_addr_struct, '0', sizeof(struct sockaddr_in));
+	sock_addr_struct->sin_family = AF_INET;
+	sock_addr_struct->sin_port = htons(port_num);
     
-    if ((temp_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((*sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Socket creation for temperature task error \n");
-        return -1;
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
     }
-    
-    if(setsockopt(temp_sockfd,SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0)
+   
+    int option = 1;
+    if(setsockopt(*sock_fd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), 
+                (char*)&option,sizeof(option)) < 0)
     {
-        printf("setsockopt failed\n");
-        close(temp_sockfd);
-        exit(2);
-    }
-	
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &temp_sock_addr.sin_addr)<=0) 
-    {
-        printf("\nInvalid address/ Address not supported for temperature task\n");
-        return -1;
-    }
-    
-    if (connect(temp_sockfd, (struct sockaddr *)&temp_sock_addr, sizeof(temp_sock_addr)) < 0)
-    {
-        printf("\nConnection Failed for temperature task \n");
-        return -1;
-    }
-    
-	memset(&light_sock_addr, '0', sizeof(light_sock_addr));
-	light_sock_addr.sin_family = AF_INET;
-	light_sock_addr.sin_port = htons(LIGHT_TASK_PORT_NUM);
-    
-    if ((light_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation for temperature task error \n");
-        return -1;
-    }
-    
-    if(setsockopt(light_sockfd,SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0)
-    {
-        printf("setsockopt failed\n");
-        close(light_sockfd);
-        exit(2);
+        perror("setsockopt failed");
+        close(*sock_fd);
+        exit(EXIT_FAILURE);
     }
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &light_sock_addr.sin_addr)<=0) 
+    if(inet_pton(AF_INET, SENSOR_TASK_SOCK_IP_ADDR, &(sock_addr_struct->sin_addr))<=0) 
     {
+        perror("inet_pton failed");
         printf("\nInvalid address/ Address not supported for temperature task\n");
-        return -1;
+        exit(EXIT_FAILURE);
     }
-    
-    if (connect(light_sockfd, (struct sockaddr *)&light_sock_addr, sizeof(light_sock_addr)) < 0)
-    {
-        printf("\nConnection Failed for light task \n");
-        return -1;
-    }
-
-	int accept_conn_id;
-	while(1){
-		
-		if ((accept_conn_id = accept(server_sockfd, (struct sockaddr *)&server_addr, 
-					   (socklen_t*)&serv_addr_len)) < 0)
-		{
-			perror("accept");
-			exit(EXIT_FAILURE);
-			
-		}
-		
-		memset(buffer, '\0', sizeof(buffer));
-		strncpy(buffer, "Hello!", strlen("Hello!"));
-		size_t sent_bytes = send(temp_sockfd, buffer , sizeof(buffer) , 0);
-		sent_bytes = send(light_sockfd, buffer , sizeof(buffer) , 0);
-		sent_bytes = send(accept_conn_id, buffer , sizeof(buffer) , 0 );
-		
-	}
-	return 0;
 }
--- Socket_Task/socket_task.h
+++ Socket_Task/socket_task.h
@@ -1,17 +1,32 @@
-/* Headers Section */
+/*************************************************************************
+* Author:       Pavan Dhareshwar & Sridhar Pavithrapu
+* Date:         03/07/2018
+* File:         light_sensor.h
+* Description:  Header file containing the macros, structs/enums, globals
+                and function prototypes for source file light_sensor.c
+*************************************************************************/
 
+#ifndef _SOCKET_TASK_H_
+#define _SOCKET_TASK_H_
+
+/*---------------------------------- INCLUDES -------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
+
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
 #include <netinet/in.h>
-#include <string.h>
 #include <arpa/inet.h>
 
-#define SERVER_PORT_NUM 			        8080
+/*---------------------------------- INCLUDES -------------------------------*/
+
+/*----------------------------------- MACROS --------------------------------*/
+#define SERVER_PORT_NUM 			        8500
 #define SERVER_LISTEN_QUEUE_SIZE	        100
 
 #define TEMPERATURE_TASK_PORT_NUM 			8081
@@ -20,8 +35,76 @@
 #define LIGHT_TASK_PORT_NUM 			    8085
 #define LIGHT_TASK_QUEUE_SIZE			    100
 
+#define SENSOR_TASK_SOCK_IP_ADDR            "127.0.0.1"   
+
 #define BUFF_SIZE                           1024
 
-/* Global variables */
+#define SOCK_REQ_MSG_API_MSG_LEN            64
+
+/*----------------------------------- MACROS --------------------------------*/
+
+/*---------------------------------- GLOBALS --------------------------------*/
 int server_sockfd, temp_sockfd, light_sockfd;
 struct sockaddr_in server_addr, temp_sock_addr, light_sock_addr;
+
+/*---------------------------------- GLOBALS --------------------------------*/
+
+/*---------------------------- STRUCTURES/ENUMERATIONS ----------------------*/
+enum _req_recipient_
+{
+    REQ_RECP_TEMP_TASK,
+    REQ_RECP_LIGHT_TASK
+};
+
+struct _socket_req_msg_struct_
+{
+    char req_api_msg[SOCK_REQ_MSG_API_MSG_LEN];
+    enum _req_recipient_ req_recipient;
+    void *ptr_param_list;
+};
+
+
+/*---------------------------- STRUCTURES/ENUMERATIONS ----------------------*/
+
+/*---------------------------- FUNCTION PROTOTYPES --------------------------*/
+
+/**
+ *  @brief Initialize server socket
+ *  
+ *  For an external application to communicate with the system, the socket 
+ *  task creates and listens on a socket for messages exposed to the external
+ *  application. This function creates, binds and makes the socket task listen 
+ *  on this socket for messages from external application
+ *
+ *  @param sock_addr_struct        : sockaddr_in structre pointer
+ *  @param port_num                : port number associated with the socket
+ *  @param listen_queue_size       : backlog argument for listen system call
+ *
+ *  @return void
+ *
+ */
+void initialize_server_socket(struct sockaddr_in *sock_addr_struct, 
+                                int port_num, int listen_queue_size);
+
+/**
+ *  @brief Initialize sensor interface socket
+ *  
+ *  For the socket task to forward the request from the external application, the 
+ *  socket task creates and uses sockets with temperature and light task to send
+ *  the requests and get the response back from the respective sensor task. This 
+ *  function creates and initializes the socket listen on this socket on the 
+ *  socket task side for communication with the respective sensor task.
+ *
+ *  @param sock_fd                 : pointer socket file descriptor
+ *  @param sock_addr_struct        : sockaddr_in structure pointer
+ *  @param port_num                : port number associated with the socket
+ *
+ *  @return void
+ *
+ */
+void initialize_sensor_task_socket(int *sock_fd, struct sockaddr_in *sock_addr_struct, 
+                                    int port_num);
+
+/*---------------------------- FUNCTION PROTOTYPES --------------------------*/
+
+#endif // _SOCKET_TASK_H_
