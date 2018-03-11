44f5e201b300c384ea26d7a6d4d68a2e66ea8cb0
Pavan Dhareshwar <pavan.dhareshwar@colorado.edu>
2018-03-10 20:21:34 -0700

    1.Made external application code single process. 2.Added initial code to test the request functionality of external application where the external application sends a request to socket task which is served and response is sent back. 3.Added a header file for external application.

--- External_Application/external_app.c
+++ External_Application/external_app.c
@@ -1,64 +1,63 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+/***************************************************************************
+* Author:       Pavan Dhareshwar & Sridhar Pavithrapu
+* Date:         03/10/2018
+* File:         external_app.c
+* Description:  Source file containing the functionality and implementation
+*               of external application
+***************************************************************************/
 
-#define SERVER_PORT_NUM 			8500
-#define SERVER_LISTEN_QUEUE_SIZE	5
-
-#define BUFF_SIZE			1024
+#include "external_app.h"
 
 int main(void)
 {
+    int client_sock;
+    struct sockaddr_in serv_addr;
+    
+    char buffer[BUFF_SIZE];
+    memset(buffer, '\0', sizeof(buffer));
+    
+    if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
 
-	pid_t child_pid;
-	child_pid = fork();
-	if (child_pid == 0)
-	{
-		/* Child Process */
-		/* Child process will be made client */
-		
-		int client_sock;
-		struct sockaddr_in serv_addr;
-		char buffer[BUFF_SIZE];
-		memset(buffer, '\0', sizeof(buffer));
-		if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		{
-				printf("\n Socket creation error \n");
-				return -1;
-		}
+    memset(&serv_addr, '0', sizeof(serv_addr));
 
-		memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT_NUM);
 
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_port = htons(SERVER_PORT_NUM);
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
-		// Convert IPv4 and IPv6 addresses from text to binary form
-		if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-		{
-				printf("\nInvalid address/ Address not supported \n");
-				return -1;
-		}
+    if (connect(client_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
 
-		if (connect(client_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-		{
-				printf("\nConnection Failed \n");
-				return -1;
-		}
-		
+    struct _socket_req_msg_struct_ ext_app_req_msg = {0};
+    strcpy(ext_app_req_msg.req_api_msg, "get_temp_data");
+    ext_app_req_msg.req_recipient = REQ_RECP_TEMP_TASK;
+    ext_app_req_msg.ptr_param_list = NULL;
 
-		/* Receiving message from parent process */
-		size_t num_read_bytes = read(client_sock, buffer, sizeof(buffer));
-		printf("Message received in child : %s\n", buffer);
-		exit(0);
-		
-	}
+    printf("Sending data to socket task\n");
+    ssize_t num_sent_bytes = send(client_sock, &ext_app_req_msg, 
+            sizeof(struct _socket_req_msg_struct_), 0);
+    if (num_sent_bytes < 0)
+    {
+        perror("send failed");
+    }
+    else
+    {
+        /* Receiving message from parent process */
+        size_t num_read_bytes = read(client_sock, buffer, sizeof(buffer));
+        printf("Message received in external app : %s\n", buffer);
+    }
 	
 	return 0;	
 }
---
+++ External_Application/external_app.h
@@ -0,0 +1,60 @@
+
+/*************************************************************************
+* Author:       Pavan Dhareshwar & Sridhar Pavithrapu
+* Date:         03/10/2018
+* File:         external_app.h
+* Description:  Header file containing the macros, structs/enums, globals
+                and function prototypes for source file external_app.c
+*************************************************************************/
+
+#ifndef _EXTERNAL_APP_H_
+#define _EXTERNAL_APP_H_
+
+/*---------------------------------- INCLUDES -------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+/*---------------------------------- INCLUDES -------------------------------*/
+
+/*----------------------------------- MACROS --------------------------------*/
+#define SERVER_PORT_NUM 			         8500
+#define SERVER_LISTEN_QUEUE_SIZE	         5
+
+#define BUFF_SIZE			                 1024
+
+#define SOCK_REQ_MSG_API_MSG_LEN             64
+
+/*----------------------------------- MACROS --------------------------------*/
+
+/*---------------------------------- GLOBALS --------------------------------*/
+
+/*---------------------------------- GLOBALS --------------------------------*/
+
+/*---------------------------- STRUCTURES/ENUMERATIONS ----------------------*/
+enum _req_recipient_
+{   
+    REQ_RECP_TEMP_TASK, 
+    REQ_RECP_LIGHT_TASK
+};  
+    
+struct _socket_req_msg_struct_
+{
+    char req_api_msg[SOCK_REQ_MSG_API_MSG_LEN];
+    enum _req_recipient_ req_recipient;
+    void *ptr_param_list;
+};
+
+/*---------------------------- STRUCTURES/ENUMERATIONS ----------------------*/
+
+/*---------------------------- FUNCTION PROTOTYPES --------------------------*/
+
+/*---------------------------- FUNCTION PROTOTYPES --------------------------*/
+
+#endif
